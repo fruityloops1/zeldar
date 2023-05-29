6b8f1f693828f6743144b394a743db44e1d549d4
GRAnimated <devin@nhscan.com>
2023-05-29 00:15:35 -0400

    fixed compilation, temp fog/brightness toggle

--- src/program/pe/DbgGui/Windows/Graphics.cpp
+++ src/program/pe/DbgGui/Windows/Graphics.cpp
@@ -28,6 +28,8 @@ namespace gui {
     namespace GraphicsHooks {
         static bool sPrePassLightState = true;
         static bool sUltraHandVoxel = true;
+        static bool sAmbientOverrideBrightness = false;
+        static bool sFog = true;
 
         void PrePassLight(gsys::ModelSceneGI *x0, agl::DrawContext *x1, gsys::ModelSceneContext *x2, gsys::RenderBufferContext &x3) {
             if (sPrePassLightState)
@@ -39,6 +41,19 @@ namespace gui {
                 __asm("LDRB W8, [X0,#8]");
             else __asm("MOV W8, #0");
         }
+
+        void AmbientOverrideBrightness() {
+            __asm("FMOV S7, S0");
+            __asm("fmov s0, %s0" : : "w"(sAmbientOverrideBrightness ? 12.0f : 1.0f));
+            __asm("FMOV S1, S0");
+            __asm("FMOV S0, S7");
+        }
+        void Fog() {
+            __asm("FMOV S7, S0");
+            __asm("FMOV S0, %s0" : : "w" (sFog ? 1.0f : 0.0f));
+            __asm("FMOV S4, S0");
+            __asm("FMOV S0, S7");
+        }
     }
 
     Graphics::Graphics() {
@@ -46,7 +61,9 @@ namespace gui {
         using namespace exl::patch::inst;
 
         Patcher(0x00A5ADDC).BranchLinkInst((void*)GraphicsHooks::PrePassLight);
-        Patcher(0x0095E814).BranchLinkInst((void*)GraphicsHooks::UltraHandVoxel);
+        Patcher(0x01279798).BranchLinkInst((void*)GraphicsHooks::AmbientOverrideBrightness);
+        Patcher(0x0096A528).BranchLinkInst((void*)GraphicsHooks::Fog);
+
     }
 
     void Graphics::update()
@@ -58,7 +75,15 @@ namespace gui {
     {
         if (getDbgGuiSharedData().showGraphics && ImGui::Begin("Graphics")) {
             ImGui::Checkbox("Pre-Pass Lights", &GraphicsHooks::sPrePassLightState);
-            ImGui::Checkbox("Ultrahand Voxels", &GraphicsHooks::sUltraHandVoxel);
+            if (ImGui::Checkbox("Ultrahand Voxels", &GraphicsHooks::sUltraHandVoxel)) {
+                exl::patch::CodePatcher(0x0095D880).Write(0x940003DD);
+            } else {
+                exl::patch::CodePatcher(0x0095D880).WriteInst(exl::armv8::inst::Nop());
+            }
+
+            ImGui::Checkbox("Override Ambient Brightness", &GraphicsHooks::sAmbientOverrideBrightness);
+            ImGui::Checkbox("Fog", &GraphicsHooks::sFog);
+
             ImGui::End();
         }
     }
